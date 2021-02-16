f4a57e1366f231547389f19595cfab9fa8806434
ben <ben.podawiltz@gmail.com>
2021-02-16 05:30:40 +0000

    Ops challenge 03 2nd function script Goals-print login history of users/one function and one variable Stretch include a function that accepts a parameter and uses it.

---
+++ print_login.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Script Name:              ops201-class03-opschall
+# Author:                   Ben Podawiltz
+# Date of last revision:    2.15.21
+# Description of purpose:   2nd function script 
+# Declaration of variables
+
+# Declaration of functions (if used)
+main () {
+    a=$(last)
+    echo "$a"
+    echo "this is a $1 script"
+    
+   
+}
+main login
+
+# End
