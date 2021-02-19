deeeb912b1b4b56b241cd1b8c4fa51c7a1f8dab7
ben <ben.podawiltz@gmail.com>
2021-02-19 07:19:21 +0000

    This is a poor draft of an array script. After spending endless hours on trying to get the logic to work using only commands, arrays, and some if/then statements I could not see it through. It does appear that a loop is the best method for the direction of this lab. I will update later in the week.

---
+++ Opschall04.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Script Name:              ops201-class04-opschall
+# Author:                   Ben Podawiltz
+# Date of last revision:    2.17.21
+# Description of purpose:   Bash array
+#
+#
+#1.Create four directories using the script.
+
+mkdir pod1 pod2 pod3 pod4
+}
+#2.Load each directory path into an array.
+function array () {
+my_dir=("pod1" "pod2" "pod3" "pod4")
+my_path="$HOME/github/gittest"
+my_files=1.txt
+}
+
+#3.Create a new .txt file by referencing the array instead of the literal filepath.
+
+ if ${my_dir[3]}
+ then touch $my_files $my_path
+ echo "it's all good"
+ if $my_files 
+ then $my_path
+ fi
+fi 
+
+
+
+
